1ce73ad2d0fd9473e6e094c7f619b99080d73506
touchspeed <47802073+touchspeed@users.noreply.github.com>
2021-01-17 11:10:55 +0800

    Update mt7620.mk
    mir3:
    sysupgrade.bin -> sysupgrade.tar

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -1065,10 +1065,10 @@ define Device/xiaomi_miwifi-r3
   KERNEL := $(KERNEL_DTB) | uImage lzma
   IMAGE_SIZE := 32768k
   UBINIZE_OPTS := -E 5
-  IMAGES := kernel1.bin rootfs0.bin sysupgrade.bin pb-boot.bin
+  IMAGES := kernel1.bin rootfs0.bin sysupgrade.tar pb-boot.bin
   IMAGE/kernel1.bin := append-kernel | check-size $$$$(KERNEL_SIZE)
   IMAGE/rootfs0.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
   IMAGE/pb-boot.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-kernel | \
         pad-to $$(KERNEL_SIZE) | append-ubi
   DEVICE_VENDOR := Xiaomi
